36fbfe68844bc8af6ec09ff0eb67cbfa8512eff0
Jin <jbjxu@thoughtworks.com>
2020-03-11 21:58:35 +0800

    test: add new test for customer statement

---
+++ src/test/java/movieRetal/CustomerTest.java
@@ -0,0 +1,35 @@
+package movieRetal;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Files;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+class CustomerTest {
+
+    private final Movie starWar = new Movie("Star War", Movie.REGULAR);
+    private final Movie tomJack = new Movie("Tom and Jack", Movie.CHILDREN);
+    private final Movie joker = new Movie("Joker", Movie.NEW_RELEASE);
+
+    @Test
+    void should_show_statements_when_customer_want_to_see_it() throws IOException {
+
+        final Customer mike = new Customer("Mike");
+        final Rental starWarRental = new Rental(starWar, 10);
+        final Rental tomJackRental = new Rental(tomJack, 20);
+        final Rental jokerRental = new Rental(joker, 30);
+
+        ImmutableList.of(starWarRental, tomJackRental, jokerRental)
+                     .forEach(mike::addRental);
+
+        final String expectedStatement = Files.toString(new File("src/test/resources/safeNet.txt"), UTF_8);
+        final String actualStatement = mike.statement();
+
+        Assertions.assertEquals(expectedStatement, actualStatement);
+    }
+}
