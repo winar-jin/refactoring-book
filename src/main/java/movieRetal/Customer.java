a4d9686ab204de4eb6bebe9a3c9da552de8e1333
Jin <jbjxu@thoughtworks.com>
2020-03-11 21:16:39 +0800

    feat: initial moving rental exercises

--- src/main/java/movieRetal/Customer.java
+++ src/main/java/movieRetal/Customer.java
@@ -7,12 +7,13 @@ import lombok.RequiredArgsConstructor;
 import java.util.Enumeration;
 import java.util.Vector;
 
+@SuppressWarnings("unchecked")
 @RequiredArgsConstructor
 public class Customer {
     @NonNull
     @Getter
     private final String _name;
-    private Vector _rentals;
+    private Vector _rentals = new Vector();
 
     public void addRental(Rental rental) {
         _rentals.addElement(rental);
