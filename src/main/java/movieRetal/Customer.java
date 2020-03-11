f74934cad8e0721d7f47a9f6f5cddfbe2c7ee185
Jin <jbjxu@thoughtworks.com>
2020-03-11 22:13:42 +0800

    refactor: extract calculate each rental by movie type

--- src/main/java/movieRetal/Customer.java
+++ src/main/java/movieRetal/Customer.java
@@ -25,44 +25,59 @@ public class Customer {
         final Enumeration rentals = _rentals.elements();
 
         String result = "Rental Record for " + get_name() + "\n";
+
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
-            Rental each = (Rental) rentals.nextElement();
+            Rental rental = (Rental) rentals.nextElement();
 
-            // determine amounts for each line
-            switch (each.get_movie().get_priceCode()) {
+            // determine amounts for rental line
+            switch (rental.get_movie().get_priceCode()) {
                 case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.get_daysRented() > 2) {
-                        thisAmount += (each.get_daysRented() - 2) * 1.5;
-                    }
+                    thisAmount = calculateRegularRentalAmount(rental);
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount += each.get_daysRented() * 3;
+                    thisAmount = calculateNewReleaseAmount(rental);
                     break;
                 case Movie.CHILDREN:
-                    thisAmount += 1.5;
-                    if (each.get_daysRented() > 3) {
-                        thisAmount += (each.get_daysRented() - 3) * 1.5;
-                    }
+                    thisAmount = calculateChildrenAmount(rental);
                     break;
             }
 
             // add frequent renter points
             frequentRentalPoints++;
             // add bonus for a two day new release rental
-            if (each.get_movie().get_priceCode() == Movie.NEW_RELEASE && each.get_daysRented() > 1) {
+            if (rental.get_movie().get_priceCode() == Movie.NEW_RELEASE && rental.get_daysRented() > 1) {
                 frequentRentalPoints++;
             }
 
             // show figures for this rental
-            result += "\t" + each.get_movie().get_title() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + rental.get_movie().get_title() + "\t" + thisAmount + "\n";
             totalAmount += thisAmount;
         }
 
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRentalPoints) + " frequent rental points";
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRentalPoints + " frequent rental points";
         return result;
+    }
+
+    private double calculateChildrenAmount(Rental rental) {
+        double thisAmount = 1.5;
+        if (rental.get_daysRented() > 3) {
+            thisAmount += (rental.get_daysRented() - 3) * 1.5;
+        }
+        return thisAmount;
+    }
+
+    private double calculateNewReleaseAmount(Rental rental) {
+        return rental.get_daysRented() * 3;
+    }
+
+    private double calculateRegularRentalAmount(Rental rental) {
+        double thisAmount = 2;
+        if (rental.get_daysRented() > 2) {
+            thisAmount += (rental.get_daysRented() - 2) * 1.5;
+        }
+        return thisAmount;
     }
 }
