2cad0c1e2b3f21f872f8e70fe5af7c20db529bc1
Jin <jbjxu@thoughtworks.com>
2020-03-11 23:00:09 +0800

    refactor: extract calculate movie amount method to Movie class

--- src/main/java/movieRetal/Rental.java
+++ src/main/java/movieRetal/Rental.java
@@ -10,35 +10,6 @@ public class Rental {
     private int _daysRented;
 
     double calculateRentalAmount() {
-        switch (get_movie().get_priceCode()) {
-            case Movie.REGULAR:
-                return calculateRegularRentalAmount();
-            case Movie.NEW_RELEASE:
-                return calculateNewReleaseAmount();
-            case Movie.CHILDREN:
-                return calculateChildrenAmount();
-            default:
-                return 0;
-        }
-    }
-
-    private double calculateNewReleaseAmount() {
-        return get_daysRented() * 3;
-    }
-
-    private double calculateChildrenAmount() {
-        double thisAmount = 1.5;
-        if (get_daysRented() > 3) {
-            thisAmount += (get_daysRented() - 3) * 1.5;
-        }
-        return thisAmount;
-    }
-
-    private double calculateRegularRentalAmount() {
-        double thisAmount = 2;
-        if (get_daysRented() > 2) {
-            thisAmount += (get_daysRented() - 2) * 1.5;
-        }
-        return thisAmount;
+        return _movie.calculateMovieAmount(get_daysRented());
     }
 }
