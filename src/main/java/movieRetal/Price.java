8d492e9a5d866be7745e7e3758dcebc75dfdf078
Jin <jbjxu@thoughtworks.com>
2020-03-12 23:17:02 +0800

    refactor: use polymorphism instead of switch in Price class

--- src/main/java/movieRetal/Price.java
+++ src/main/java/movieRetal/Price.java
@@ -3,33 +3,22 @@ package movieRetal;
 abstract class Price {
     abstract int getPriceCode();
 
-    double getCharge(int daysRented) {
-        double thisAmount = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2) {
-                    thisAmount += (daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDREN:
-                thisAmount += 1.5;
-                if (daysRented > 3) {
-                    thisAmount += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
-    }
+    abstract double getCharge(int daysRented);
 }
 
 class ChildrenPrice extends Price {
     @Override
     int getPriceCode() {
         return Movie.CHILDREN;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double thisAmount = 1.5;
+        if (daysRented > 3) {
+            thisAmount += (daysRented - 3) * 1.5;
+        }
+        return thisAmount;
     }
 }
 
@@ -38,11 +27,25 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
 
 class RegularPrice extends Price {
     @Override
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double thisAmount = 2;
+        if (daysRented > 2) {
+            thisAmount += (daysRented - 2) * 1.5;
+        }
+        return thisAmount;
     }
 }
