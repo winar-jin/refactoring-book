01db92689bd27d5c77287adf15db120b303708ab
Jin <jbjxu@thoughtworks.com>
2020-03-12 23:10:32 +0800

    refactor: move get charge method to Price class

--- src/main/java/movieRetal/Movie.java
+++ src/main/java/movieRetal/Movie.java
@@ -34,25 +34,7 @@ public class Movie {
     }
 
     double getCharge(int daysRented) {
-        double thisAmount = 0;
-        switch (getPrice()) {
-            case REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2) {
-                    thisAmount += (daysRented - 2) * 1.5;
-                }
-                break;
-            case NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case CHILDREN:
-                thisAmount += 1.5;
-                if (daysRented > 3) {
-                    thisAmount += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
+        return price.getCharge(daysRented);
     }
 
     private int getPrice() {
